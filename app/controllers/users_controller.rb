bdd629b258d7142d76c4dad5bc8e333d121d4646
Davids Chijioke Prince <davids.chijioke@gmail.com>
2021-04-28 18:08:55 +0100

    friends user show profile function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
@@ -9,6 +10,11 @@ class UsersController < ApplicationController
 
   def search
     render json: params[:friend] 
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 
   def search
